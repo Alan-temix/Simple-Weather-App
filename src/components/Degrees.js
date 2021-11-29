4e8c4128efcdcb90343c4b7c265e07c463a0fa0d
Alan Temix <Temix@protonmail.com>
2021-11-29 00:40:25 -0600

    Diseño cambiado

--- src/components/Degrees.js
+++ src/components/Degrees.js
@@ -1,26 +1,72 @@
 import {useState} from 'react';
 
-const Degrees = ({temp}) => {
-    const [degrees, setDegrees] = useState(temp);
+const Degrees = ({temp, weatherDescription, icon, tempMax, tempMin, windspeed, sunriseUTC, sunsetUTC, clouds}) => {
     const [celcius, setCelcius] = useState(true);
+
+    const kelvinToCelcius = (kelvin) => {
+        let result = (kelvin - 273).toFixed();
+        return `${result} °C`;
+    }
+
+    const kelvinToFartenheit = (kelvin) => {
+        let result = (((kelvin - 273.15) * 1.8) + 32).toFixed();
+        return `${result} °F`;
+    }
+
+    const getTimeUTCtoStr = (param) => {
+        let timeUTC = new Date(param * 1000);
+        let timeSunriseOrSunsetStr = timeUTC.toLocaleTimeString();
+        return timeSunriseOrSunsetStr;
+    }
     
     return(
-        <div className="col-6">
-            <p className="card-text">{degrees}{celcius ? " °C" : " °F"}</p>
-            <button type="button" className="btn btn-primary" onClick={() => {
-                if(celcius === true) {
-                    setDegrees((degrees * 1.8) + 32)
-                    setCelcius(false);
-                }
-                if(celcius === false) {
-                    setDegrees((degrees - 32) / 1.8)
-                    setCelcius(true)
-                }
-            }}>Degrees 
-            <br />
-            °F / °C
-            </button>
+        <div className="d-flex">
+            <div className="col-md-6 flex" style={{borderRight: '1px solid gray'}}>
+                <img src={icon} className="" alt="" style={{maxWidth: '300px'}} />
+                <div className="" style={{alignSelf: 'center'}}>
+                    <p className="card-text" style={{fontSize: 'xx-large'}}>{celcius ? `${kelvinToCelcius(temp)}` : `${kelvinToFartenheit(temp)}`}</p>
+                    <p>{weatherDescription}</p>
+                    <button type="button" className="btn btn-primary" onClick={() => {
+                        if(celcius === true) {
+                            setCelcius(false);
+                        }
+                        if(celcius === false) {
+                            setCelcius(true)
+                        }
+                    }}>Degrees 
+                    <br />
+                    °C / °F
+                    </button>
+                </div>
+            </div>
+            <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', width: '100%', padding: '0 20px'}}>
+                <div>
+                    <p>{celcius ? `${kelvinToCelcius(tempMax)}` : `${kelvinToFartenheit(tempMax)}`}</p>
+                    <span>High</span>
+                </div>
+                <div>
+                    <p>{windspeed} m/s</p>
+                    <span>Windspeed</span>
+                </div>
+                <div>
+                    <p>{getTimeUTCtoStr(sunriseUTC)}</p>
+                    <span>Sunrise</span>
+                </div>
+                <div>
+                    <p>{celcius ? `${kelvinToCelcius(tempMin)}` : `${kelvinToFartenheit(tempMin)}`}</p>
+                    <span>Low</span>
+                </div>
+                <div>
+                    <p>{clouds}</p>
+                    <span>Clouds</span>
+                </div>
+                <div>
+                    <p>{getTimeUTCtoStr(sunsetUTC)}</p>
+                    <span>Sunset</span>
+                </div>
+            </div>
         </div>
+
     )
 }
 
--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -3,6 +3,12 @@ import Degrees from './Degrees';
 
 const WeatherContainer = ({ latitude, longitude }) => {
     const [data, setData] = useState(null);
+    const [weatherUnit, setWeatherUnit] = useState('celcius');
+    const date = new Date();
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
 
     useEffect(() => {
         if(latitude !== 0 && longitude !== 0) {
@@ -16,31 +22,60 @@ const WeatherContainer = ({ latitude, longitude }) => {
       }, [latitude, longitude])
 
     if(data != null) {
-        const capitalizeFirstLetter = (string) => {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-          }
-
+        console.log(data);
         let city = data['name'];
         let country = data['sys']['country'];
         let sky = data['weather'][0]['description'];
-        let icon = `https://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`;
+        let icon = `https://openweathermap.org/img/wn/${data['weather'][0]['icon']}@4x.png`;
         let windspeed = data['wind']['speed']
-        let temp = (data['main']['temp'] / 10).toFixed(1);
+        let temp = data['main']['temp'];
+        let tempMax = data['main']['temp_max'];
+        let tempMin = data['main']['temp_min'];
         let pressure = data['main']['pressure'];
         let humidity = data['main']['humidity'];
         let clouds = `${data['clouds']['all']} %`
+        // 
+        let sunriseUTC = data['sys']['sunrise'];
+        let sunsetUTC = data['sys']['sunset'];
+
         return(
             <div className="card">
                 <div className="card-body">
-                    <h2 className="card-title">Weather APP</h2>
-                    <h5>{`${city}/${country}`}</h5>
-                    <div className="first-container">
-                        <div className="col-6">
-                        "{capitalizeFirstLetter(sky)}"
-                        <img src={icon} className="card-img-top" alt="" style={{width: '5rem'}} />
-                        </div>
-                        <Degrees temp={temp}/>
+                    <div>
+                        <h2 className="card-title">{`${city} ${country}`}</h2>
+                        <p>{date.toUTCString()}</p>
                     </div>
+                    <div>
+                    {/* Inicio search */}
+                        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        Search other city
+                        </button>
+
+                        <div className="modal fade text-dark" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="staticBackdropLabel">Search other location</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <div className="row">
+                                        <label htmlFor="colFormLabelLg" className="col-sm-4 col-form-label col-form-label-lg">Location:</label>
+                                        <div className="col-sm-8">
+                                            <input type="email" className="form-control form-control-lg" id="colFormLabelLg" placeholder="Type here location" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary">Search</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Fin search */}
+                    </div>
+                    <Degrees temp={temp} tempMax={tempMax} tempMin={tempMin} weatherDescription={capitalizeFirstLetter(sky)} icon={icon} windspeed={windspeed} sunriseUTC={sunriseUTC} sunsetUTC={sunsetUTC} clouds={clouds} />
                     <p>Windspeed: {windspeed} m/s</p>
                     <p>Pressure: {pressure} mb</p>
                     <p>Humidity: {humidity}</p>
